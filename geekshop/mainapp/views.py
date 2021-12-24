73bcf0a0dcdf18db0a5c979e1c3f63fb37841dda
Yuriy <kireev_juriyy@rambler.ru>
2021-12-24 17:10:00 +0800

    Done task_3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,30 +1,57 @@
 from django.shortcuts import render
-from datetime import datetime
+from .models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
+
+MENU_LINKS = [
+        {'href': 'index', 'name': 'домой'},
+        {'href': 'products', 'name': 'продукты'},
+        {'href': 'contact', 'name': 'контакты'},
+    ]
 
 def index(request):
+    # products = [
+    #     {
+    #         'name': 'Квантовая лампа',
+    #         'description': 'Светодиодная лампа',
+    #         'image_path': 'img/product-1.jpg',
+    #     },
+    #     {
+    #         'name': 'Стул синий',
+    #         'description': 'Стул со спинкой мягкий, синий.',
+    #         'image_path': 'img/product-2.jpg',
+    #     }
+    # ]
+    products = Product.objects.all()
     return render(request, 'mainapp/index.html', context={
-        'message': 'GeekSHOP',
+        'title': 'Магазин',
+        'content_block_class': 'slider',
+        'menu_links': MENU_LINKS,
+        'products': products,
     })
 
 
 def contact(request):
-    menu_links = [
-        {'href': 'index', 'name': 'домой'},
-        {'href': 'products', 'name': 'продукты'},
-        {'href': 'contact', 'name': 'контакты'},
-    ]
+
     return render(request, 'mainapp/contact.html', context={
-        'menu_links': menu_links
+        'title': 'Контакты',
+        'content_block_class': 'hero',
+        'menu_links': MENU_LINKS
     })
 
 
-def products(request):
-    menu_links = [
-        {'href': 'index', 'name': 'домой'},
-        {'href': 'products', 'name': 'продукты'},
-        {'href': 'contact', 'name': 'контакты'},
-    ]
+def products(request, pk=None):
+    if not pk:
+        select_category = ProductCategory.objects.first()
+    else:
+        select_category = get_object_or_404(ProductCategory, id=pk)
+    categories = ProductCategory.objects.all()
+    products = Product.objects.filter(category=select_category)
     return render(request, 'mainapp/products.html', context={
-        'menu_links': menu_links
+        'title': 'Каталог',
+        'content_block_class': 'hero-white',
+        'menu_links': MENU_LINKS,
+        'select_category': select_category,
+        'categories': categories,
+        'products': products,
     })
 
