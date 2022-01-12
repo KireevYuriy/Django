8e8e2fe1ff1cc771aa575602ad3863353e0c1b63
Yuriy <kireev_juriyy@rambler.ru>
2022-01-13 01:49:09 +0800

    create task_7

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = "mainapp"
-
 urlpatterns = [
     path("", views.products_index, name="index"),
     path('<int:pk>/', views.products, name='category'),
